0c1a0e97511acdf666427990028ed450cf774df0
Kanika Sharma <kanikash4@gmail.com>
2017-11-23 15:07:21 +0530

    Additions

---
+++ src/client/components/viewResult/viewResult.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import React from 'react';
+import './viewResult.scss';
+import 'whatwg-fetch';
+
+class finalResult extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (<div>
+                    <div className="resultTable">
+                        <tr>ROW1</tr>
+                    </div>
+                </div>
+        );
+    }
+}
+
+export default finalResult;
--- src/client/lib/urls.js
+++ src/client/lib/urls.js
@@ -1,3 +1,2 @@
 const domainURL = 'http://localhost:7080';
 export const submitNumber = domainURL + '/submitNumber';
-export const getResult = domainURL + '/getResult';
